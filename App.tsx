3747032e38ef7dc4797d107b509c06226b0349e0
unknown <rickywijaya2004@gmail.com>
2025-01-17 00:16:00 +0700

    update deploy

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
 import { store } from './src/store';
 import { AppNavigator } from './src/navigation/index';
+import { app } from './src/services/firebase/config';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
